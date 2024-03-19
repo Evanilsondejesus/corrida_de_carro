8f56d9575158ac699ed6492323d89bd8e48f8c1c
evanilon de jesus <evaevanilson@gmail.com>
2024-03-19 17:45:46 -0300

    pista foi criada

--- script.js
+++ script.js
@@ -1,11 +1,81 @@
 const game = document.querySelector("#game");
 
-
  
-setInterval(() => {
-let car = document.createElement("div")
-car.classList.add("cars")
 
-game.appendChild(car)
+
+
+
+
+var corrida= 0;
+  
+setInterval(() => { 
+
+    const cars = document.querySelectorAll(".cars")
+
+
+    //var irmaos = game.children;
+
+
+
+for (let corrida = 0; corrida < cars.length; corrida++) {
+var car_top = parseInt(window.getComputedStyle(cars[corrida]).getPropertyValue('top'))
+var car_lef = parseInt(window.getComputedStyle(cars[corrida]).getPropertyValue('left'))
+
+ cars[corrida].style.top = (car_top+10)+"px"
+ 
+
+
+
+
+   if(car_top > 900){
+    console.log(car_lef)
+
+
     
-}, 1000);
+   var novosObastaculos = document.createElement("div");
+
+
+   novosObastaculos.classList.add("cars")
+
+if(car_lef == 318){
+console.log("sim estou muito aqui")
+novosObastaculos.style.top = "-100px"
+
+}else{
+    novosObastaculos.style.top = "-100px"
+
+    novosObastaculos.style.left = "925px"
+
+
+}
+
+
+
+   
+    
+
+game.appendChild(novosObastaculos)
+
+cars[corrida].remove()
+
+}
+
+    
+}
+
+
+
+
+     
+// const car = document.querySelector("#car");
+
+
+    
+//     var top_car = parseInt(window.getComputedStyle(car).getPropertyValue('top'))
+// console.log(top_car)
+
+// corrida+=10
+// car.style .top= `${corrida}px`
+
+
+}, 100);
